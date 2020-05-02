63985c5e8dd1f99bc484b7fbe6145d240d0b8d89
HelpSeeker <AlmostSerious@protonmail.ch>
2020-05-02 21:31:45 +0200

    Adjust copyright/license notices
    This is done in accordance with the FSF's instructions on how to use GNU
    licenses for one's software.

--- coub-gui.py
+++ coub-gui.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python3
 
+"""
+Copyright (C) 2018-2020 HelpSeeker <AlmostSerious@protonmail.ch>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
 import os
 from textwrap import dedent
 
@@ -82,7 +99,24 @@ def translate_to_cli(options):
                     'name': 'CoubDownloader',
                     'description': 'A simple download script for coub.com',
                     'website': 'https://github.com/HelpSeeker/CoubDownloader',
-                    'license': 'GPLv3',
+                    'license': dedent(
+                        """
+                        Copyright (C) 2018-2020 HelpSeeker <AlmostSerious@protonmail.ch>
+
+                        This program is free software: you can redistribute it and/or modify
+                        it under the terms of the GNU General Public License as published by
+                        the Free Software Foundation, either version 3 of the License, or
+                        (at your option) any later version.
+
+                        This program is distributed in the hope that it will be useful,
+                        but WITHOUT ANY WARRANTY; without even the implied warranty of
+                        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+                        GNU General Public License for more details.
+
+                        You should have received a copy of the GNU General Public License
+                        along with this program.  If not, see <https://www.gnu.org/licenses/>.
+                        """
+                    ),
                 }
             ]
         }
